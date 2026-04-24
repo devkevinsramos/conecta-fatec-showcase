bd09f271559dadfedaa02d6628b2e8982bd10c16
Kevin S Ramos <profissionalkevinsramos@gmail.com>
2026-04-23 23:06:20 -0300

    Update index

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Alternância simples entre tema claro e escuro
+const themeToggle = document.getElementById("themeToggle");
+const body = document.body;
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("theme-light");
+  body.classList.toggle("theme-dark");
+
+  if (body.classList.contains("theme-dark")) {
+    themeToggle.textContent = "☀️";
+  } else {
+    themeToggle.textContent = "🌙";
+  }
+});
